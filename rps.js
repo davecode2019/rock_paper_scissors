cca58649248c4a599f796d684a9be54ef60e8f55
davecode2019 <dave.code2019@gmail.com>
2022-07-11 07:33:26 +0100

    Added comments to JS code

--- rps.js
+++ rps.js
@@ -2,6 +2,7 @@
 // Author: David Watkin
 
 const computerPlay = () => {
+  // Randomly picks 0, 1 or 2 and returns an appropriate string value.
   let computerChoice = Math.floor(Math.random() * 3);
   switch (computerChoice) {
     case 0:
@@ -17,6 +18,9 @@ const computerPlay = () => {
 };
 
 const playRound = (playerSelection) => {
+  // Takes players choice, generates a computer random response, passes to calculateWinner
+  // function to decide winner. Based on winner, generates text for display in GUI and
+  // increments the appropriate score. Finally passes all information to the updateScreen function.
   let computerSelection = computerPlay();
   let result = calculateWinner(playerSelection, computerSelection);
   let winner = "";
@@ -39,6 +43,8 @@ const playRound = (playerSelection) => {
 };
 
 const calculateWinner = (playerSelection, computerSelection) => {
+  // Calculates the winner of a round based on selection based conditions.
+  // Returns a number value indicating winner.
   if (playerSelection === computerSelection) {
     return 0; // 0 = Draw
   }
@@ -64,6 +70,8 @@ const calculateWinner = (playerSelection, computerSelection) => {
 };
 
 const updateScreen = (playerSelection, computerSelection, winner) => {
+  // Updates the GUI at the end of each round, showing the results and ongoing scores.
+  // Passes off to the checkGameOver function to check if an end game condition has been met.
   const infoText = document.querySelector(".info-text");
   infoText.textContent = resultText;
 
@@ -74,11 +82,13 @@ const updateScreen = (playerSelection, computerSelection, winner) => {
   computerScoreText.textContent = computerScore;
 
   const playerToken = document.querySelector(".player-token");
-  playerToken.textContent = playerSelection[0];
+  playerToken.textContent = playerSelection[0]; // Just first letter of choice displayed.
 
   const computerToken = document.querySelector(".computer-token");
-  computerToken.textContent = computerSelection[0];
+  computerToken.textContent = computerSelection[0]; // Just first letter of choice displayed.
 
+  // Highlights the winner of each round using css clas token-win.
+  // Previous rounds highlight removed prior to adding new winner.
   computerToken.classList.remove("token-win");
   playerToken.classList.remove("token-win");
   if (winner === "computer") {
@@ -92,6 +102,11 @@ const updateScreen = (playerSelection, computerSelection, winner) => {
 };
 
 const checkGameOver = () => {
+  // Checks to see if either player has achieved a score of 5.
+  // If 5 has been achieved, choice buttons are removed from GUI
+  // and replaced with text detailing winner.
+  // A play again button is placed at the bottom of the screen,
+  // replaces instructional text. Pressing button reloads the GUI to initial state.
   let gameWinner = "";
   if (playerScore === 5) {
     gameWinner = "PLAYER";
@@ -103,7 +118,7 @@ const checkGameOver = () => {
 
   if (gameWinner) {
     const buttonArea = document.querySelector(".button-area");
-    buttonArea.textContent = "";
+    buttonArea.textContent = ""; // Clear contents of div (selection buttons)
 
     const winnerText = document.createElement("p");
     winnerText.textContent = `${gameWinner} WINS!`;
@@ -116,7 +131,7 @@ const checkGameOver = () => {
     const playAgain = document.createElement("button");
     playAgain.textContent = "PLAY AGAIN?";
     playAgain.classList.add("play-again");
-    playAgain.addEventListener("click", () => document.location.reload());
+    playAgain.addEventListener("click", () => document.location.reload()); // Reload GUI.
     infoArea.appendChild(playAgain);
   }
 };
